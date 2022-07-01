0243ea0f545b013728238a266edb620dff09a7f1
m-smith15 <mpsmith1215@gmail.com>
2022-07-01 11:00:48 -0400

    cleaning up js

--- DojoWeather/scripts.js
+++ DojoWeather/scripts.js
@@ -7,51 +7,28 @@ function acceptCookies(){
     document.querySelector('.footer').remove();
 }
 
+var highDegreeNums = document.querySelectorAll(".high");
+console.log(highDegreeNums);
+
+var lowDegreeNums = document.querySelectorAll(".low");
+console.log(highDegreeNums);
+
 function degreeChange(){
     //(C × 9/5) + 32 = F
     currentSelection = document.querySelector('.unitsChange option:checked').innerText
     // console.log(currentSelection);
+    // default is F, so first will check F -> C 
     if(currentSelection == "C"){
-        //F -> C
-        // var lowChange = (parseInt(low,10) % 1.8) - 32;
-        // var highChange = (parseInt(high,10) % 1.8) - 32;
-        // console.log('change from f to c')
-        high1 = document.querySelector('.highs1').innerText;
-        high2 = document.querySelector('.highs2').innerText;
-        high3 = document.querySelector('.highs3').innerText;
-        high4 = document.querySelector('.highs4').innerText;
-        low1 = document.querySelector('.lows1').innerText;
-        low2 = document.querySelector('.lows2').innerText;
-        low3 = document.querySelector('.lows3').innerText;
-        low4 = document.querySelector('.lows4').innerText;
-        document.querySelector(".highs1").innerText = (parseInt(high1,10) % 1.8) - 32;
-        document.querySelector(".highs2").innerText = (parseInt(high2,10) % 1.8) - 32;
-        document.querySelector(".highs3").innerText = (parseInt(high3,10) % 1.8) - 32;
-        document.querySelector(".highs4").innerText = (parseInt(high4,10) % 1.8) - 32;
-        document.querySelector(".lows1").innerText = (parseInt(low1,10) % 1.8) - 32;
-        document.querySelector(".lows2").innerText = (parseInt(low2,10) % 1.8) - 32;
-        document.querySelector(".lows3").innerText = (parseInt(low3,10) % 1.8) - 32;
-        document.querySelector(".lows4").innerText = (parseInt(low4,10) % 1.8) - 32;
+        for(var x=0;x<highDegreeNums.length;x++){
+            highDegreeNums[x].innerText = Math.round((parseInt(highDegreeNums[x].innerHTML,10) - 32) * (5/9));
+            lowDegreeNums[x].innerText = Math.round((parseInt(lowDegreeNums[x].innerHTML,10) - 32) * (5/9));
+        }
     }
+    //added this should the option go to C back to F 
     else{
-        // var lowChange = (parseInt(low,10) * 1.8) + 32;
-        // var highChange = (parseInt(high,10) * 1.8) + 32;
-        // console.log('changed from c to f')
-        high1 = document.querySelector('.highs1').innerText;
-        high2 = document.querySelector('.highs2').innerText;
-        high3 = document.querySelector('.highs3').innerText;
-        high4 = document.querySelector('.highs4').innerText;
-        low1 = document.querySelector('.lows1').innerText;
-        low2 = document.querySelector('.lows2').innerText;
-        low3 = document.querySelector('.lows3').innerText;
-        low4 = document.querySelector('.lows4').innerText;
-        document.querySelector(".highs1").innerText = (parseInt(high1,10) % 1.8) - 32;
-        document.querySelector(".highs2").innerText = (parseInt(high2,10) % 1.8) - 32;
-        document.querySelector(".highs3").innerText = (parseInt(high3,10) % 1.8) - 32;
-        document.querySelector(".highs4").innerText = (parseInt(high4,10) % 1.8) - 32;
-        document.querySelector(".lows1").innerText = (parseInt(low1,10) % 1.8) - 32;
-        document.querySelector(".lows2").innerText = (parseInt(low2,10) % 1.8) - 32;
-        document.querySelector(".lows3").innerText = (parseInt(low3,10) % 1.8) - 32;
-        document.querySelector(".lows4").innerText = (parseInt(low4,10) % 1.8) - 32;
+        for(var x=0;x<lowDegreeNums.length;x++){
+            highDegreeNums[x].innerText = Math.round((parseInt(highDegreeNums[x].innerHTML,10) * (5/9)) + 32);
+            lowDegreeNums[x].innerText = Math.round((parseInt(lowDegreeNums[x].innerHTML,10) * (5/9)) + 32);
+        }
     }
 }
